a4a8a3727b579c9770b8f892fa0ba29a92a6ce67
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-04-25 08:20:21 -0400

    *actually* fix modules

--- .syncRepos.js
+++ .syncRepos.js
@@ -1,14 +1,26 @@
 fs = require('fs');
 const { exec } = require("child_process");
 
-function checkFileExists(file) {
-	return fs.promises.access(file, fs.constants.F_OK)
-			 .then(() => true)
-			 .catch(() => false)
-}
+fs.access('./.git', fs.F_OK, (err) => {
+	if (err) {
+		console.log('.git not found, cloning repo')
 
+		exec('git clone https://github.com/inertia-unblocker/uv-scripts ./server/static/uv', (err, stdout, stderr) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			if (stderr) {
+				console.error(stderr);
+				return;
+			}
+			console.log(stdout);
+		});
 
-if (checkFileExists('./.git/')) {
+		return
+	}
+
+	console.log('.git found, using submodules')
 	exec('git submodule update --init', (err, stdout, stderr) => {
 		if (err) {
 			console.error(err);
@@ -20,16 +32,4 @@ if (checkFileExists('./.git/')) {
 		}
 		console.log(stdout);
 	});
-} else {
-	exec('git clone https://github.com/inertia-unblocker/uv-scripts ./server/static/uv', (err, stdout, stderr) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		if (stderr) {
-			console.error(stderr);
-			return;
-		}
-		console.log(stdout);
-	});
-}
+});
