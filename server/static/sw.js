5e4d7c6abdcec5cd2b1ce0e2cb784c3fdee02a82
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-04-27 16:47:13 -0400

    indentation

--- server/static/sw.js
+++ server/static/sw.js
@@ -3,7 +3,7 @@ importScripts('./uv/uv.sw.js');
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
+	event.respondWith(
+		sw.fetch(event)
+	)
 );
