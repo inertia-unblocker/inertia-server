ce1a44956011bb0ea66d56e694a4909e50bd5763
Doomcow500 <74429751+Doomcow500@users.noreply.github.com>
2022-06-02 07:08:36 +0000

    Update index.js

--- server/static/index.js
+++ server/static/index.js
@@ -4,7 +4,7 @@ window.addEventListener('load', async event => {
 	let url = new URLSearchParams(document.location.search).get('url');
 	
 	if (url == null || url == '' || url == ' ' || typeof(url) == 'undefined') {
-		window.location.href = 'https://inertia-ub.vercel.app/';
+		window.location.href = 'https://inertia-unblocker.vercel.app/';
 	}
 	
 	window.navigator.serviceWorker.register('./sw.js', {
