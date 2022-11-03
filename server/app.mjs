88919f267148f81400d282db3670e2e027e70909
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-11-03 15:16:00 -0400

    fix ultraviolet

--- server/app.mjs
+++ server/app.mjs
@@ -1,11 +1,8 @@
 import express from 'express';
-import path from 'path';
-import * as url from 'url';
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'static')));
-app.get('/toInertia', (req, res) => res.redirect(process.env.INERTIA));
+app.get('/toInertia', (_req, res) => res.redirect(process.env.INERTIA));
+app.use(express.static('./server/static'));
 app.listen(port, () => console.log(`Listening on port ${port}`));
